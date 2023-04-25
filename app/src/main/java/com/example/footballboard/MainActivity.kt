691a71b6977f206432df60b11fe85d662ff848a4
docfr10 <docfr10@yandex.ru>
2023-04-25 18:09:30 +0300

    Created project

---
+++ app/src/main/java/com/example/footballboard/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.footballboard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.footballboard.ui.theme.FootballBoardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FootballBoardTheme {
+
+            }
+        }
+    }
+}
