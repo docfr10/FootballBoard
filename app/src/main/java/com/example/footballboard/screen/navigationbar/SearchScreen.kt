7fae8f79890bd7db24f00848e81fce4d325b1572
docfr10 <docfr10@yandex.ru>
2023-04-27 11:02:11 +0300

    Fixed a search screen markup

--- app/src/main/java/com/example/footballboard/screen/navigationbar/SearchScreen.kt
+++ app/src/main/java/com/example/footballboard/screen/navigationbar/SearchScreen.kt
@@ -11,9 +11,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,10 +27,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.footballboard.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,9 +58,10 @@ fun SearchScreen(window: Window, context: Context) {
             Modifier
                 .fillMaxSize()
                 .padding(paddingValues = paddingValues),
-            verticalArrangement = Arrangement.Top
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(Modifier.fillMaxWidth()) {
+            Row {
                 // OutlinedTextField to type the team name
                 OutlinedTextField(
                     value = teamName.value,
@@ -70,6 +77,43 @@ fun SearchScreen(window: Window, context: Context) {
                 )
                 IconButton(onClick = { /*TODO*/ }) {
                     Icon(imageVector = Icons.Default.Search, contentDescription = "Search button")
+                }
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(horizontal = 10.dp, vertical = 10.dp)
+            ) {
+                item {
+                    Card(modifier = Modifier.fillMaxWidth()) {
+                        Row(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+                            Column(
+                                verticalArrangement = Arrangement.SpaceAround,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Info,
+                                    contentDescription = "Home team logo"
+                                )
+                                Text(text = "Home team name")
+                            }
+                            Text(text = "0-0", fontSize = 40.sp)
+                            Column(
+                                verticalArrangement = Arrangement.SpaceAround,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Info,
+                                    contentDescription = "Guest team logo"
+                                )
+                                Text(text = "Guest team name")
+                            }
+                        }
+                    }
                 }
             }
         }
